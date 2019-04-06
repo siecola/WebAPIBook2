f6d3066c891e713a5adf21b5230e308256751ca6
siecola <siecola@gmail.com>
2019-04-06 14:52:07 -0300

    Projeto Exemplo2

--- Exemplo2/Controllers/ValuesController.cs
+++ Exemplo2/Controllers/ValuesController.cs
@@ -1,28 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Exemplo1.Controllers
+namespace Exemplo2.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
         public IEnumerable<string> Get()
         {
-            Trace.TraceInformation("INFO - Get all values");
-            Trace.TraceWarning("WARN - Get all values");
-            Trace.TraceError("ERROR - Get all values");
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            Trace.TraceInformation("INFO - Get one value: " + id);
             return "value";
         }
 
---
+++ Exemplo2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Exemplo2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        [Required]
+        public string codigo { get; set; }
+
+        public decimal preco { get; set; }
+    }
+}
